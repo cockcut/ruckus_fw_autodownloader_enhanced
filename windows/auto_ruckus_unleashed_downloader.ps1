017ff0244591db86495f1d62ed2dd8c185afce70
cockcut <105777311+cockcut@users.noreply.github.com>
2026-08-21 15:02:13 +0900

    Update auto_ruckus_unleashed_downloader.ps1

--- windows/auto_ruckus_unleashed_downloader.ps1
+++ windows/auto_ruckus_unleashed_downloader.ps1
@@ -1,5 +1,5 @@
 ﻿# =================================================================
-# Ruckus Unleashed Firmware Auto Downloader for Windows (v2.1.6)
+# Ruckus Unleashed Firmware Auto Downloader for Windows (v2.1.0)
 # - Parallel Downloading Support (Max 3 Concurrent)
 # =================================================================
 
